480d84baa3dedb6222a8a7759bf3ee60e32d7eb5
sandipumbare90 <160105096+sandipumbare90@users.noreply.github.com>
2025-06-26 17:53:37 +0530

    Update provider.tf

--- terraform-infra/gke-infra/provider.tf
+++ terraform-infra/gke-infra/provider.tf
@@ -1,6 +1,6 @@
 provider "google" {
-  project = var.gcp_project_id
-  region  = var.gcp_region # e.g., "us-central1" or "europe-west1"
+  project = var.project_id
+  region  = var.region # e.g., "us-central1" or "europe-west1"
   impersonate_service_account = "cicd-sa@mydevopsproject-464107.iam.gserviceaccount.com"
 }
 
